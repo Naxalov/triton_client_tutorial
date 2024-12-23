167e4f36e709339f49c79c5b5e36989e6dc86214
Zarifjon Nakhalov <naxalov@MacBook-Air-Zarifjon.local>
2024-12-23 14:50:04 +0500

    load the simple model

---
+++ get_models.sh
@@ -0,0 +1,4 @@
+# blob:https://github.com/548b574a-081f-455c-b96f-43f56a438ace
+# Create simple directory
+mkdir simple
+curl -o simple/model.graphdef https://raw.githubusercontent.com/triton-inference-server/server/refs/heads/main/docs/examples/model_repository/simple/1/model.graphdef
